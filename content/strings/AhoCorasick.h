83a97ba42c2bfbceb14692e0edf7759247e0c43a
karrigan01 <106960373+karrigan01@users.noreply.github.com>
2024-11-14 22:09:59 +0700

    Add files via upload

--- content/strings/AhoCorasick.h
+++ content/strings/AhoCorasick.h
@@ -17,69 +17,49 @@
  */
 #pragma once
 
-struct AhoCorasick {
-	enum {alpha = 26, first = 'A'}; // change this!
-	struct Node {
-		// (nmatches is optional)
-		int back, next[alpha], start = -1, end = -1, nmatches = 0;
-		Node(int v) { memset(next, v, sizeof(next)); }
-	};
-	vector<Node> N;
-	vi backp;
-	void insert(string& s, int j) {
-		assert(!s.empty());
-		int n = 0;
-		for (char c : s) {
-			int& m = N[n].next[c - first];
-			if (m == -1) { n = m = sz(N); N.emplace_back(-1); }
-			else n = m;
-		}
-		if (N[n].end == -1) N[n].start = j;
-		backp.push_back(N[n].end);
-		N[n].end = j;
-		N[n].nmatches++;
-	}
-	AhoCorasick(vector<string>& pat) : N(1, -1) {
-		rep(i,0,sz(pat)) insert(pat[i], i);
-		N[0].back = sz(N);
-		N.emplace_back(0);
+struct aho
+{
+    struct TrieNode
+    {
+        int nxt[26], link;
+    };
+    vector<TrieNode> Trie;
+    vector<int> adj[100005];
+    int nTree;
 
-		queue<int> q;
-		for (q.push(0); !q.empty(); q.pop()) {
-			int n = q.front(), prev = N[n].back;
-			rep(i,0,alpha) {
-				int &ed = N[n].next[i], y = N[prev].next[i];
-				if (ed == -1) ed = y;
-				else {
-					N[ed].back = y;
-					(N[ed].end == -1 ? N[ed].end : backp[N[ed].start])
-						= N[y].end;
-					N[ed].nmatches += N[y].nmatches;
-					q.push(ed);
-				}
-			}
-		}
-	}
-	vi find(string word) {
-		int n = 0;
-		vi res; // ll count = 0;
-		for (char c : word) {
-			n = N[n].next[c - first];
-			res.push_back(N[n].end);
-			// count += N[n].nmatches;
-		}
-		return res;
-	}
-	vector<vi> findAll(vector<string>& pat, string word) {
-		vi r = find(word);
-		vector<vi> res(sz(word));
-		rep(i,0,sz(word)) {
-			int ind = r[i];
-			while (ind != -1) {
-				res[i - sz(pat[ind]) + 1].push_back(ind);
-				ind = backp[ind];
-			}
-		}
-		return res;
-	}
+    void insertString(const string &str) {
+        int pt(0);
+        for (char ch : str) {
+            int c(ch - 'a');
+            if(!Trie[pt].nxt[c]) {
+                Trie[pt].nxt[c] = Trie.size();
+                Trie.emplace_back();
+            }
+            pt = Trie[pt].nxt[c];
+        }
+    }
+
+    void buildAutomaton(void) {
+        queue<int> qu;
+        qu.push(0);
+        while(qu.size()) {
+            int v(qu.front()), u(Trie[v].link);
+            qu.pop();
+
+            for (int c = 0; c < 26; ++c) {
+                if(Trie[v].nxt[c]) {
+                    Trie[Trie[v].nxt[c]].link = (v) ? Trie[u].nxt[c] : 0;
+                    qu.push(Trie[v].nxt[c]);
+                } else {
+                    Trie[v].nxt[c] = Trie[u].nxt[c];
+                }
+            }
+        }
+    }
+    void buildTree(void) {
+        nTree = Trie.size();
+        for (int i = 1; i < nTree; ++i)
+            adj[Trie[i].link].push_back(i);
+    }
+
 };
