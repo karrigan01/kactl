cfdbd06d50c4ff4d612b52c55330095be528ce81
karrigan01 <106960373+karrigan01@users.noreply.github.com>
2024-11-14 17:46:33 +0700

    Update template.cpp

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,11 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+#define all(fl) fl.begin(),fl.end()
+#define pb push_back
+#define fi first
+#define se second
+#define for1(i,j,k) for(int i=j;i<=k;i++)
+#define for2(i,j,k) for(int i=j;i>=k;i--)
+#define for3(i,j,k,l) for(int i=j;i<=k;i+=l)
+#define lb lower_bound
+#define ub upper_bound
+#define sz(a) (int)a.size()
+#define pii pair<int,int>
+#define pli pair<long long,int>
+#define gcd __gcd
+#define lcm(x,y) x*y/__gcd(x,y)
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
-#define all(x) begin(x), end(x)
-#define sz(x) (int)(x).size()
 typedef long long ll;
-typedef pair<int, int> pii;
 typedef vector<int> vi;
 
 int main() {
